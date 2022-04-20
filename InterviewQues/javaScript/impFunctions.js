1981c07bfb61d1c5c343c67e9882372964cbdf35
Swati Zarekar <szarekar@upperlakeps.com>
2022-04-20 16:43:45 -0400

    updates

--- InterviewQues/javaScript/impFunctions.js
+++ InterviewQues/javaScript/impFunctions.js
@@ -8,28 +8,33 @@ console.log(surname)
 console.log(typeof str)
 console.log(typeof surname)
 
+/*toUpperCase() */
 console.log(str.toUpperCase())
+/*toLowerCase() */
 console.log(str.toLowerCase())
+/*includes() */
 console.log(str.includes('Awesome'))
-
+/*split */
 console.log(str.split(" "))
 let words = str.split(' ')
 words.forEach(ele => {
     console.log(ele);
 })
-
+/*length */
 console.log(str.length)
-
+/*trim */
 let space = '         Hey Swati         '
 console.log(space.trim())
+/*trimLeft */
 console.log(space.trimLeft())
+/*trimRight */
 console.log(space.trimRight())
-
+/*charAt(position)*/
 console.log(str.charAt(0))
 
 let len = str.length
 console.log(str.charAt(len - 1))
-
+/*subString */
 let email = "swatiupr01@habemcodev.com"
 splitEmail = email.substring(0, 9)
 console.log(splitEmail)
---
+++ InterviewQues/javaScript/stringFun.js
@@ -0,0 +1,28 @@
+//now.endswith & now.startsWith
+let now = 'Stay home, Work from Home'
+console.log(now.startsWith('Stay'))
+console.log(now.endsWith('e'))
+//now.search
+console.log(now.search('e'))
+//now.includes
+console.log(now.includes('o'))
+
+// join two tables Concatation
+let firstName = 'swati'
+let lastName = ' Zarekar'
+console.log(firstName.concat(lastName))
+let fullName = firstName.concat(lastName)
+console.log(fullName)
+
+//equal == ====
+let one = 'apple'; // literal
+let two = new String('apple') // object
+console.log(one === two);
+console.log(one == two);
+
+let num = 1; //number
+let numStr = '1' //string
+
+console.log(num == numStr);
+console.log(num === numStr);
+
